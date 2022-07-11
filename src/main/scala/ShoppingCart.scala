7c1304ba96b27e63103d8a168d768ff0599c4b10
paulthor <3472481+PaulThor@users.noreply.github.com>
2022-07-11 20:41:12 +0100

    Replace product strings with types

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,28 +1,26 @@
 package main.scala
 
+trait Product {
+    val price : BigDecimal
+}
+
+case object Apple extends Product {
+    val price = 0.60
+}
+case object Orange extends Product {
+    val price = 0.25
+}
+
 object ShoppingCart {
 
-    def calcTotalPrice(shoppingList: String*) = {
-        shoppingList.map {
-                case "Apple" => 0.60
-                case "Orange" => 0.25
-                case _ => throw new Exception("Invalid Product") 
-        }.sum
+    def calcTotalPrice(shoppingList: Product*) = {
+        shoppingList.map{ x => x.price }.sum
     }
 
-    def calcTotalPriceWithDiscount(shoppingList: String*) = {
+    def calcTotalPriceWithDiscount(shoppingList: Product*) = {
 
-        calcDiscount(
-            2,
-            1,
-            0.6,
-            shoppingList.filter(_ match { case "Apple" => true case _ => false }).size) +
-        calcDiscount(
-            3, 
-            2, 
-            0.25, 
-            shoppingList.filter(_ match { case "Orange" => true case _ => false }).size
-        )
+        calcDiscount(2, 1, 0.6, shoppingList.filter(_ match { case Apple => true case _ => false }).size) +
+        calcDiscount(3, 2, 0.25, shoppingList.filter(_ match { case Orange => true case _ => false }).size)
     }
 
     def calcDiscount(n : Int, m : Int, cost: BigDecimal, totalItem : Int) : BigDecimal = cost * (totalItem / n * m + totalItem % n)
--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -1,28 +1,24 @@
 package test.scala
 
 import org.scalatest._
-import main.scala.ShoppingCart
+import main.scala.{ ShoppingCart, Apple, Orange }
 
 class ShoppingCartSpec extends wordspec.AnyWordSpec {
 
   "Shopping cart calculation" should {
     "return 2.05" in {
-      val totalPrice = ShoppingCart.calcTotalPrice("Apple", "Apple","Orange", "Apple")
+      val totalPrice = ShoppingCart.calcTotalPrice(Apple, Apple, Orange, Apple)
       assert(totalPrice == 2.05)
     }
 
     "return 0.60" in {
-         val totalPrice = ShoppingCart.calcTotalPrice("Apple")
+         val totalPrice = ShoppingCart.calcTotalPrice(Apple)
         assert(totalPrice == 0.60)
     }
 
     "return 0.85" in {
-         val totalPrice = ShoppingCart.calcTotalPrice("Apple", "Orange")
+         val totalPrice = ShoppingCart.calcTotalPrice(Apple, Orange)
         assert(totalPrice == 0.85)
-    }
-
-    "throw Invalid Product exception" in {
-        assertThrows[Exception](ShoppingCart.calcTotalPrice("Banana"))
     }
   }
 
@@ -34,37 +30,37 @@ class ShoppingCartSpec extends wordspec.AnyWordSpec {
     }
 
     "calculate 1 apples" in {
-      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Apple")
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount(Apple)
       assert(totalPrice == 0.60)
     }
 
     "calculate 2 apples" in {
-      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Apple", "Apple")
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount(Apple, Apple)
       assert(totalPrice == 0.60)
     }
 
     "calculate 3 apples" in {
-      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Apple", "Apple", "Apple")
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount(Apple, Apple, Apple)
       assert(totalPrice == 1.20)
     }
 
     "calculate 4 apples" in {
-      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Apple", "Apple", "Apple", "Apple")
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount(Apple, Apple, Apple, Apple)
       assert(totalPrice == 1.20)
     }
 
     "calculate 3 oranges" in {
-      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Orange", "Orange", "Orange")
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount(Orange, Orange, Orange)
       assert(totalPrice == 0.50)
     }
 
     "calculate 4 oranges" in {
-      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Orange", "Orange", "Orange", "Orange")
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount(Orange, Orange, Orange, Orange)
       assert(totalPrice == 0.75)
     }
 
     "calculate a mixture of products" in {
-      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple")
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount(Apple, Orange, Orange, Apple, Orange, Orange, Apple)
       assert(totalPrice == 1.95)
     }
 
