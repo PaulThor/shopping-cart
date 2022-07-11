667227c75b17783049f945500674f60bac46827d
paulthor <3472481+PaulThor@users.noreply.github.com>
2022-07-11 15:07:03 +0100

    Added shopping cart discount

--- src/test/scala/ShoppingCart.scala
+++ src/test/scala/ShoppingCart.scala
@@ -9,4 +9,21 @@ object ShoppingCart {
                 case _ => throw new Exception("Invalid Product") 
         }.sum
     }
+
+    def calcTotalPriceWithDiscount(shoppingList: String*) = {
+
+        calcDiscount(
+            shoppingList.filter(_ match { case "Apple" => true case _ => false }).size,
+            2,
+            0.6) +
+        calcDiscount(
+            shoppingList.filter(_ match { case "Orange" => true case _ => false }).size,
+            3,
+            0.25)
+    }
+
+    def calcDiscount(quantity: Int, mod: Int, price: BigDecimal) = {
+         val eq = (quantity + 1) - ((quantity + 1) % mod)
+        (eq / mod * price)
+    }
 }
--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -6,18 +6,62 @@ import hmrc.exercise.ShoppingCart
 class ShoppingCartSpec extends wordspec.AnyWordSpec {
 
   "Shopping cart calculation" should {
-    "return £2.05" in {
+    "return 2.05" in {
       val totalPrice = ShoppingCart.calcTotalPrice("Apple", "Apple","Orange", "Apple")
       assert(totalPrice == 2.05)
     }
 
-    "return £0.60" in {
+    "return 0.60" in {
          val totalPrice = ShoppingCart.calcTotalPrice("Apple")
         assert(totalPrice == 0.60)
+    }
+
+    "return 0.85" in {
+         val totalPrice = ShoppingCart.calcTotalPrice("Apple", "Orange")
+        assert(totalPrice == 0.85)
     }
 
     "throw Invalid Product exception" in {
         assertThrows[Exception](ShoppingCart.calcTotalPrice("Banana"))
     }
+  }
+
+  "Shopping cart with discount calculation" should {
+
+    "calculate 0 apples" in {
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount()
+      assert(totalPrice == 0.0)
+    }
+
+    "calculate 1 apples" in {
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Apple")
+      assert(totalPrice == 0.60)
+    }
+
+    "calculate 2 apples" in {
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Apple", "Apple")
+      assert(totalPrice == 0.60)
+    }
+
+    "calculate 3 apples" in {
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Apple", "Apple", "Apple")
+      assert(totalPrice == 1.20)
+    }
+
+    "calculate 4 apples" in {
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Apple", "Apple", "Apple", "Apple")
+      assert(totalPrice == 1.20)
+    }
+
+    "calculate 3 oranges" in {
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Orange", "Orange", "Orange")
+      assert(totalPrice == 0.25)
+    }
+
+    "calculate 4 oranges" in {
+      val totalPrice = ShoppingCart.calcTotalPriceWithDiscount("Orange", "Orange", "Orange", "Orange")
+      assert(totalPrice == 0.25)
+    }
+
   } 
 }
